82f0ba4087f4e235560acd6cc8e9e96833bcb7ea
myyiko <33124301+myyiko@users.noreply.github.com>
2018-04-09 18:02:24 +0800

    magic python
    人生苦短 我用python

--- magic_python.py
+++ magic_python.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# 将两个dict合并(python > 3.4)
+# 将两个dict合并(python > 3.4) 效率奇高
 # 这个语法不仅合并  还能去重(如果key和value的值一致的话) 如果key相同,会将第一个元素中的key-value移除  保留最后元素的
 d1 = {'a': 1}
 d2 = {'b': 2}
