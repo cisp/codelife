60e16b88c2b2e5db30def469d19ccea79ddfe8ff
= <15618620929@163.com>
2018-04-13 14:35:12 +0800

    magic python

--- magic_python.py
+++ magic_python.py
@@ -14,3 +14,53 @@ now = datetime.now()
 
 print(str(now))
 print(repr(now))
+
+# dict排序 可根据key/value/key or value length and so on
+
+d1 = {'n': 21, 'h': 'sd', 'a': 213}
+
+d1 = sorted(d1.items(), key=lambda d: d[0])  # sort by key
+
+# 上面的lammbda函数可以使用operator.itemgger代替
+
+import operator
+
+d1 = sorted(d1.items(), key=operator.itemgger(1))  # sort by value
+
+from collections import OrderedDict
+
+# return OrderedDict object(imp by dict)
+od = OrderedDict(d1)  # 最直接的方法  使用轮子 这个轮子牛逼在于可以按照排序后的顺序添加
+
+od['b'] = 123  # 'b': 123 在元素最末尾 保证顺序
+
+
+# 动态  真正的动态:所谓动态 就是一边做的时候  一边获取  python 厉害就厉害在这里(我是觉得真的强)
+# 1.根据对象 获取对象的属性(这个简单  动态语言都有的特性)
+class A:
+    def __init__(self, name):
+        self.name = name
+
+    def __call__(self, *args, **kwargs):
+        self.name = self.name
+
+    def test(self):
+        print('for test!')
+
+    def hello(self):
+        print('hello world!')
+
+
+a = A('python')
+print(a.__class__.__name__)  # 获取当前类名
+print(a.__dict__)  # 获取字段属性
+hasattr(a, 'hello')
+getattr(a, 'hello')  # 这是真正的神器 如果对象a有属性hello 那就获取这个属性
+
+
+# 下面就厉害了 动态获取模块中的属性
+import importlib
+
+m1 = importlib.import_module('test')
+if hasattr(m1, 'A'):
+    print('模块test中存在属性A')
